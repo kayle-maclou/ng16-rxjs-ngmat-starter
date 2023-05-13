7e478a89a078220f0bd36a8f3cad45e65068a7bf
Kayle Maclou <kayle.maclou@gmail.com>
2023-05-13 15:14:36 +0200

    Created Angular 16 project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-domain-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng16-starter-rxjs-ngmat';
+}
