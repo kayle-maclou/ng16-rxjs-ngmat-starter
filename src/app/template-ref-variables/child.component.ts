8d298c9ee329e2d76d2b71846c043d0b41ff25d3
Kayle Maclou <kayle.maclou@gmail.com>
2023-08-30 16:10:45 +0200

    Added component IO example

---
+++ src/app/template-ref-variables/child.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'component-io-child',
+  template: `
+    <p>
+      child component works with my {{myInputParam}}.
+      <button (click)="clickMe()">Click me!</button>
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TemplateRefVariables_ChildComponent {
+  @Input()
+  myInputParam: string = '';
+
+  @Output()
+  myOutputEvent = new EventEmitter<string>();
+
+  clickMe() {
+    this.myOutputEvent.emit('Child was clicked!');
+  }
+
+}
